64d9b33d20432d07269539a65feb210916a869fd
Cloud068 <84558271+JYFC068@users.noreply.github.com>
2026-04-03 04:24:12 +0800

    Delete COMP3820SEF-GP directory

--- COMP3820SEF-GP/src/main/java/com/group/onlinecourse/repository/PollOptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group.onlinecourse.repository;
-
-import com.group.onlinecourse.entity.PollOption;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PollOptionRepository extends JpaRepository<PollOption, Long> {
-    List<PollOption> findByPollId(Long pollId);
-}
